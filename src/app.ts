7a2dbfd132186a1655fd9c6e827e2c2928f8e506
shahin1290 <s.patowary@yahoo.com>
2022-11-26 09:33:31 +0100

    setup mongodb and config env

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Express } from "express";
 import { WeMeetServer } from "./setupServer";
+import databaseConnection from "./setupDatabase";
+import { config } from "./config";
 
 class Application {
   public initialize(): void {
+    this.loadConfig();
+    databaseConnection();
     const app: Express = express();
     const server: WeMeetServer = new WeMeetServer(app);
     server.start();
+  }
+
+  private loadConfig(): void {
+    config.validateConfig();
   }
 }
 
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -7,6 +7,7 @@ import hpp from "hpp";
 import cookieSession from "cookie-session";
 import HTTP_STATUS from "http-status-codes";
 import "express-async-errors";
+import { config } from "./config";
 
 const SERVER_PORT = 5000;
 
@@ -29,16 +30,16 @@ export class WeMeetServer {
     app.use(
       cookieSession({
         name: "session",
-        keys: ["test1", "test2"],
+        keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_TWO!],
         maxAge: 24 * 7 * 3600000,
-        secure: false,
+        secure: config.NODE_ENV !== "development",
       })
     );
     app.use(hpp());
     app.use(helmet());
     app.use(
       cors({
-        origin: "*",
+        origin: config.CLIENT_URL,
         credentials: true,
         optionsSuccessStatus: 200,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -52,7 +53,7 @@ export class WeMeetServer {
   }
   private routeMidleware(app: Application): void {}
   private globalErrorHandler(app: Application): void {}
-  
+
   private async startServer(app: Application): Promise<void> {
     try {
       const httpServer: http.Server = new http.Server(app);
